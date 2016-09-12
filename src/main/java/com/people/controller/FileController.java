b1d3c12eb9d3db4eee506d52f37a98ba4246c9f0
1106285275 <1106285275@qq.com>
2016-09-12 21:11:52 +0800

    test2

--- src/main/java/com/people/controller/FileController.java
+++ src/main/java/com/people/controller/FileController.java
@@ -13,7 +13,7 @@ public class FileController {
 
    @RequestMapping("/name")
     public String getFileName(){
-        System.out.println("dgsdf");
+        System.out.println("dgsdfddd");
         return "filename.....";
     }
 }
